ae61703f080b4b7decefaa009c2abb6f0720cc09
Darkli <139178176+D4rkli@users.noreply.github.com>
2026-06-18 00:29:17 +0300

    Update settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
     "default": dj_database_url.config(
         default=os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
         conn_max_age=600,
-        ssl_require=os.getenv("DB_SSL_REQUIRE", "False").lower() in {"1","true","yes"},
+        ssl_require=True,
     )
 }
 
